ab7b5235f9f8d202c60c006cfb49c343729f9f7e
gus3000 <gus3000spam@gmail.com>
2024-03-25 12:58:03 +0100

    feat: snap camera to cardinal direction

--- src/Direction.cs
+++ src/Direction.cs
@@ -1,9 +1,54 @@
-﻿namespace DungeonCrawler;
+﻿using System;
+using Godot;
+
+namespace DungeonCrawler;
 
 public enum Direction
 {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+    NORTH, // -z
+    EAST, // +x
+    SOUTH, // +z
+    WEST, // -x
+}
+
+public static class DirectionHelper
+{
+    public static Vector3 GetOffset(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.NORTH => new Vector3(0, 0, -1),
+            Direction.EAST => new Vector3(1, 0, 0),
+            Direction.SOUTH => new Vector3(0, 0, 1),
+            Direction.WEST => new Vector3(-1, 0, 0),
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+
+    public static float GetAimedYAngle(Camera3D camera)
+    {
+        var direction = FromCamera(camera);
+        var angle = (-(int)direction * Mathf.Tau / 4 + Mathf.Tau) % Mathf.Tau;
+        
+        // if diff > 180,  reduce diff to < 180
+        while (Mathf.Abs(angle - camera.Rotation.Y) > Mathf.Pi)
+        {
+            angle -= Mathf.Tau * Mathf.Sign(angle - camera.Rotation.Y);
+        }
+
+        return angle;
+    }
+
+    public static Direction FromCamera(Camera3D camera)
+    {
+        var angle = -camera.GlobalRotation.Y + Mathf.Tau;
+        var rounded = Mathf.RoundToInt(4 * angle / Mathf.Tau) % 4;
+
+        return (Direction)rounded;
+    }
+
+    public static Vector3 GetHorizontalVector(Camera3D camera)
+    {
+        return -new Vector3(0, 0, 1).Rotated(Vector3.Up, camera.Rotation.Y);
+    }
 }
--- src/PlayerController.cs
+++ src/PlayerController.cs
@@ -5,8 +5,15 @@ namespace DungeonCrawler;
 public partial class PlayerController : Node3D
 {
     public Direction CurrentDirection { get; protected set; }
+
+    protected Camera3D Camera;
+    protected Node3D DebugObject;
+
     public override void _Ready()
     {
+        Camera = GetNode<Camera3D>("Camera3D");
+        DebugObject = GetNode<Node3D>("Debug");
+
         GD.Print("Player ready");
         SetCurrentDirectionFromCamera();
     }
@@ -14,10 +21,31 @@ public partial class PlayerController : Node3D
     public override void _Process(double delta)
     {
         SetCurrentDirectionFromCamera();
+        if (Input.IsActionJustPressed("snap"))
+        {
+            SnapCamera();
+        }
     }
+
+
+    protected void SnapCamera() => SnapCamera(CurrentDirection);
+
+    protected void SnapCamera(Direction direction)
+    {
+        Camera.RotateY(0); // Reset rotation between -180 and 180
+        
+        Tween tween = GetTree().CreateTween();
+        tween.SetProcessMode(Tween.TweenProcessMode.Idle); //on frame update
+        tween.TweenProperty(Camera,
+            "rotation",
+            new Vector3(0, DirectionHelper.GetAimedYAngle(Camera), 0),
+            0.5f
+        );
+    }
+
 
     protected void SetCurrentDirectionFromCamera()
     {
-        CurrentDirection = Direction.NORTH;
+        CurrentDirection = DirectionHelper.FromCamera(Camera);
     }
 }
