398981b8fe582f60cef877796c364e487606a415
gus3000 <gus3000spam@gmail.com>
2024-03-27 19:45:01 +0100

    feat: new painting system using texture blending

--- src/Wall.cs
+++ src/Wall.cs
@@ -16,21 +16,28 @@ public partial class Wall : Node3D
         GD.Print("wall plane :", _wallPlane);
 
         _splashImage = _splashTexture.GetImage();
+        _splashImage.Decompress();
+        // _splashImage.Convert(Image.Format.Rgba8);
 
         _mat = _wallPlane.GetActiveMaterial(0) as BaseMaterial3D;
         GD.Print("mat : ", _mat);
-        // mat.AlbedoTexture.
+
+        // _img = _mat.AlbedoTexture.GetImage().Duplicate() as Image;
+        // _img.Decompress();
+
         _img = Image.Create(1024, 1024, false, Image.Format.Rgba8);
         Random rnd = new Random();
         _img.Fill(new Color(
-            rnd.NextSingle(),
-            rnd.NextSingle(),
-            rnd.NextSingle()
+            1,
+            1,
+            1
         ));
+
         _mat.AlbedoTexture = ImageTexture.CreateFromImage(_img);
         UpdateTexture();
 
-        RandomSplash();
+        for (int i = 0; i < 100; i++)
+            RandomSplash();
     }
 
     private void UpdateTexture()
@@ -44,7 +51,7 @@ public partial class Wall : Node3D
     public override void _Process(double delta)
     {
         base._Process(delta);
-        RandomSplash();
+        // RandomSplash();
     }
 
     public void RandomSplash()
@@ -56,13 +63,12 @@ public partial class Wall : Node3D
             rnd.Next(_img.GetHeight())
         );
 
-        // Vector2I splashDim = _splashImage.GetSize();
-        Vector2I splashDim = new Vector2I(128,128);
-        // GD.Print("splashing at ", splashPoint, " with dim ", splashDim);
+        Vector2I splashDim = _splashImage.GetSize();
+        // GD.Print("splash point : ", splashPoint, ", splash dim : ", splashDim);
+        // Vector2I splashDim = new Vector2I(128,128);
 
-        _img.FillRect(new Rect2I(splashPoint, splashDim), new Color(rnd.NextSingle(), rnd.NextSingle(), rnd.NextSingle()));
-        // _img.Rect
-        // _img.BlitRect(_splashImage, new Rect2I(splashPoint, splashDim), splashDim);
+        // GD.Print("tex format :", (_mat.AlbedoTexture as ImageTexture).GetFormat(), ", splash format : ", _splashImage.GetFormat());
+        _img.BlendRect(_splashImage, new Rect2I(0, 0, splashDim), splashPoint);
         UpdateTexture();
     }
 }
